1f3c7d9559b03bee6b371e2971c71b941e9e9058
hafizhfy <60763122+hafizhf@users.noreply.github.com>
2021-03-24 20:39:23 +0700

    Update 0.13
    Adjusting and customizing

--- js/statistic.js
+++ js/statistic.js
@@ -2,7 +2,7 @@
 
 window.onload = function () {
 
-    var chart = new CanvasJS.Chart("ip-semester", {
+    var chart1 = new CanvasJS.Chart("chart-ip-semester", {
         animationEnabled: true,
         theme: "light2",
 
@@ -55,7 +55,7 @@ window.onload = function () {
             ]
         }]
     });
-    chart.render();
+    chart1.render();
 
     function toogleDataSeries(e) {
         if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
@@ -63,129 +63,130 @@ window.onload = function () {
         } else {
             e.dataSeries.visible = true;
         }
-        chart.render();
+        chart1.render();
     }
+
+
+
+    // CHART NILAI TUGAS PER PERTEMUAN ----------------------
+
+    var chart2 = new CanvasJS.Chart("chart-assignment-result", {
+        theme: "light2",
+        animationEnabled: true,
+        axisY: {
+            title: "Nilai"
+        },
+        toolTip: {
+            shared: "true"
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "spline",
+            showInLegend: true,
+            
+            name: "BTI",
+            dataPoints: [
+                { label: "Pertemuan 2", y: 90 },
+                { label: "Pertemuan 3", y: 86 },
+                { label: "Pertemuan 4", y: 94 },
+                { label: "Pertemuan 6", y: 86 },
+                { label: "Pertemuan 7", y: 89 }
+            ]
+        },
+        {
+            type: "spline",
+            showInLegend: true,
+            
+            name: "Sistem Operasi",
+            dataPoints: [
+                { label: "Pertemuan 1", y: 88 },
+                { label: "Pertemuan 2", y: 87 },
+                { label: "Pertemuan 3", y: 90 },
+                { label: "Pertemuan 4", y: 94 },
+                { label: "Pertemuan 5", y: 92 },
+                { label: "Pertemuan 6", y: 84 },
+                { label: "Pertemuan 7", y: 86 }
+            ]
+        },
+        {
+            type: "spline",
+            showInLegend: true,
+            
+            name: "PBO",
+            dataPoints: [
+                { label: "Pertemuan 3", y: 90 },
+                { label: "Pertemuan 4", y: 91 },
+                { label: "Pertemuan 6", y: 89 }
+            ]
+        },
+        {
+            type: "spline",
+            showInLegend: true,
+            
+            name: "Pemrograman Web",
+            dataPoints: [
+                { label: "Pertemuan 3", y: 90 },
+                { label: "Pertemuan 4", y: 83 },
+                { label: "Pertemuan 5", y: 88 },
+                { label: "Pertemuan 7", y: 92 }
+            ]
+        },
+        {
+            type: "spline",
+            showInLegend: true,
+            
+            name: "KPL",
+            dataPoints: [
+                { label: "Pertemuan 1", y: 92 },
+                { label: "Pertemuan 5", y: 90 },
+                { label: "Pertemuan 6", y: 91 }
+            ]
+        },
+        {
+            type: "spline",
+            showInLegend: true,
+            
+            name: "Teknologi Basis Data",
+            dataPoints: [
+                { label: "Pertemuan 2", y: 89 },
+                { label: "Pertemuan 3", y: 90 },
+                { label: "Pertemuan 4", y: 91 },
+                { label: "Pertemuan 5", y: 92 },
+                { label: "Pertemuan 6", y: 85 }
+            ]
+        },
+        {
+            type: "spline",
+            showInLegend: true,
+            
+            name: "Jaringan Komputer",
+            dataPoints: [
+                { label: "Pertemuan 1", y: 85 },
+                { label: "Pertemuan 2", y: 89 },
+                { label: "Pertemuan 3", y: 84 },
+                { label: "Pertemuan 4", y: 86 },
+                { label: "Pertemuan 5", y: 90 },
+                { label: "Pertemuan 6", y: 90 }
+            ]
+        }]
+    });
+    chart2.render();
+
+
+
+    function toggleDataSeries(e) {
+        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        chart2.render();
+    }
+    
+    
 
 }
 
-
-
-// CHART NILAI TUGAS PER PERTEMUAN ----------------------
-
-// window.onload = function () {
-
-//     var chart = new CanvasJS.Chart("assignment-result", {
-//         theme: "light2",
-//         animationEnabled: true,
-//         axisY: {
-//             title: "Nilai"
-//         },
-//         toolTip: {
-//             shared: "true"
-//         },
-//         legend: {
-//             cursor: "pointer",
-//             itemclick: toggleDataSeries
-//         },
-//         data: [{
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "BTI",
-//             dataPoints: [
-//                 { label: "Pertemuan 2", y: 90 },
-//                 { label: "Pertemuan 3", y: 86 },
-//                 { label: "Pertemuan 4", y: 94 },
-//                 { label: "Pertemuan 6", y: 86 },
-//                 { label: "Pertemuan 7", y: 89 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "Sistem Operasi",
-//             dataPoints: [
-//                 { label: "Pertemuan 1", y: 88 },
-//                 { label: "Pertemuan 2", y: 87 },
-//                 { label: "Pertemuan 3", y: 90 },
-//                 { label: "Pertemuan 4", y: 94 },
-//                 { label: "Pertemuan 5", y: 92 },
-//                 { label: "Pertemuan 6", y: 84 },
-//                 { label: "Pertemuan 7", y: 86 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "PBO",
-//             dataPoints: [
-//                 { label: "Pertemuan 3", y: 90 },
-//                 { label: "Pertemuan 4", y: 91 },
-//                 { label: "Pertemuan 6", y: 89 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "Pemrograman Web",
-//             dataPoints: [
-//                 { label: "Pertemuan 3", y: 90 },
-//                 { label: "Pertemuan 4", y: 83 },
-//                 { label: "Pertemuan 5", y: 88 },
-//                 { label: "Pertemuan 7", y: 92 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "KPL",
-//             dataPoints: [
-//                 { label: "Pertemuan 1", y: 92 },
-//                 { label: "Pertemuan 5", y: 90 },
-//                 { label: "Pertemuan 6", y: 91 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "Teknologi Basis Data",
-//             dataPoints: [
-//                 { label: "Pertemuan 2", y: 89 },
-//                 { label: "Pertemuan 3", y: 90 },
-//                 { label: "Pertemuan 4", y: 91 },
-//                 { label: "Pertemuan 5", y: 92 },
-//                 { label: "Pertemuan 6", y: 85 }
-//             ]
-//         },
-//         {
-//             type: "spline",
-//             showInLegend: true,
-            
-//             name: "Jaringan Komputer",
-//             dataPoints: [
-//                 { label: "Pertemuan 1", y: 85 },
-//                 { label: "Pertemuan 2", y: 89 },
-//                 { label: "Pertemuan 3", y: 84 },
-//                 { label: "Pertemuan 4", y: 86 },
-//                 { label: "Pertemuan 5", y: 90 },
-//                 { label: "Pertemuan 6", y: 90 }
-//             ]
-//         }]
-//     });
-//     chart.render();
-
-//     function toggleDataSeries(e) {
-//         if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-//             e.dataSeries.visible = false;
-//         } else {
-//             e.dataSeries.visible = true;
-//         }
-//         chart.render();
-//     }
-
-// }
